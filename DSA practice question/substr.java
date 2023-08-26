1c910f52e7b8866ec686ae58524b778ac9a466af
GAGAN MAKHIJA <87359955+gaganmakhija@users.noreply.github.com>
2023-08-26 19:03:52 +0530

    Add files via upload
    this is the solution of program in which you find the substring from the given string

---
+++ DSA practice question/substr.java
@@ -0,0 +1,8 @@
+// progrm to find the substring in the given string we can solve it by the help of build in function .substring
+
+public class substr {
+    public static void main(String[] args) {
+        String name ="GAGAN MAKHIJA IS A  SOFTWARE DEVELOPER";
+        System.out.println(name.substring(0, 16));
+    }
+}
